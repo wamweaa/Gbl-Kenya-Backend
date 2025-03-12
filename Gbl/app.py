498e0b8e988b2213c82a543ca8f9b91c2008dedf
wamweaa <patokinya12@gmail.com>
2025-03-12 21:58:34 +0300

    payment

--- Gbl/app.py
+++ Gbl/app.py
@@ -16,6 +16,15 @@ import cloudinary.uploader
 from config import MPESA_SHORTCODE, MPESA_PASSKEY, MPESA_CALLBACK_URL, MPESA_BASE_URL
 from your_module import get_mpesa_token  # Import the token function
 import base64
+MPESA_CONSUMER_KEY = "hV8s2GQfEjGfzEWq504mHkGbPm1FtpE2t7KI6asKuyEd50KS"
+MPESA_CONSUMER_SECRET = "WgNofqiscvyxmBxpTZFrEC5nF1nVfFDFBjtL01LlYhetWpANK9tfyaU8JsBiGlEi"
+MPESA_SHORTCODE = "852648"
+MPESA_PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+MPESA_CALLBACK_URL = "https://gblkenya.com/mpesa_callback"
+MPESA_BASE_URL = "https://api.safaricom.co.ke"
+
+
+
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__, static_folder="frontend/build", static_url_path="/")
