04e5d732a456322f57d2966e6942c05885e7200c
wamweaa <patokinya12@gmail.com>
2025-03-21 20:47:31 +0300

    pagtination

--- Gbl/app.py
+++ Gbl/app.py
@@ -450,12 +450,24 @@ def get_users():
 @app.route('/products', methods=['GET'])
 def list_products():
     category = request.args.get('category')
+    page = request.args.get('page', default=1, type=int)
+    per_page = request.args.get('per_page', default=10, type=int)
+    
     query = Product.query
     if category:
         query = query.filter_by(category=category)
-    products = query.all()
-    return jsonify([product.to_dict() for product in products])
-
+    
+    # Paginate the query
+    paginated_products = query.paginate(page=page, per_page=per_page, error_out=False)
+    
+    # Return the paginated results along with pagination metadata
+    return jsonify({
+        'products': [product.to_dict() for product in paginated_products.items],
+        'total_pages': paginated_products.pages,
+        'current_page': paginated_products.page,
+        'total_products': paginated_products.total
+    })
+    
 @app.route('/products/<int:id>', methods=['GET'])
 def get_product_by_id(id):
     product = Product.query.get_or_404(id)
